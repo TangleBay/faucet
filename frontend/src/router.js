c87c33020e84ffe8bfb9c31a4c8014be3e7186d6
no8ody <no8ody@tanglebay.com>
2020-05-07 22:38:51 +0200

    -

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,8 +5,6 @@ import Home from './views/Home.vue';
 Vue.use(Router);
 
 export default new Router({
-	mode: 'history',
-	hash: false,
 	scrollBehavior(to, from, savedPosition) {
 		if (savedPosition) {
 			return savedPosition;
@@ -21,18 +19,9 @@ export default new Router({
 			component: Home
 		},
 		{
-			path: '/about',
-			name: 'about',
-			// route level code-splitting
-			// this generates a separate chunk (about.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-		},
-		{
 			path: '/fill',
 			name: 'fill',
 			component: () => import(/* webpackChunkName: "fill" */ './views/Fill.vue')
 		},
-		{ path: '*', redirect: '/' }, // catch all use case
 	]
 });
